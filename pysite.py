75ec5f19c39dc8942f7bb9f2e7053659f07662f3
TooFooDev <144743612+toofoodev@users.noreply.github.com>
2025-05-26 13:39:11 -0500

    PySite v1.1

--- pysite.py
+++ pysite.py
@@ -1,10 +1,12 @@
 # PySite  |  Wes Parker
+# PS (v1.1)
 # PySite Load
-print("PySite Engine 0.1\n")
+print("PySite Engine 1")
+print("(Made by Wes/TooFooDev)\n")
 # Web Brouser
 def web():
     print("Welcome to PySite!")
-    print("Type a site to go to it.\n")
+    print("Type a site to go to it.")
     inputweb = input("")
 
     # PySite:
@@ -17,11 +19,19 @@ def web():
         print("\n")
         print("PySite: pysite.py/poop")
         pysite_poop_indexpy()
+        
+    # Wes:
     if inputweb == "wes.py":
         print("\n")
         print("PySite: wes.py")
         wes_indexpy()
+        
+    if inputweb == "wes.py/wesisgoofy":
+        print("\n")
+        print("PySite: wes.py/wesisgoofy")
+        wes_wesisgoofy_indexpy()
     
+    print("\n" + inputweb + " is not valid!" + "\n")
     web()
 # Python Websites
 # PySite:
@@ -38,6 +48,33 @@ def wes_indexpy():
     print("WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES WES")
     print("\n")
     web()
+def wes_wesisgoofy_wrongpy():
+    # PySite:Hidden
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG")
+    print("WRONG\n")
+    web()
+def wes_wesisgoofy_indexpy(): 
+    print("Wes is very goofy.")
+    print("Is Wes goofy?")
+    inputuser = input("Yes or no?\n")
+    if inputuser == "yes":
+        print("Good!\n")
+        web()
+    else:
+        wes_wesisgoofy_wrongpy()
 
 # Run Site
 web()
